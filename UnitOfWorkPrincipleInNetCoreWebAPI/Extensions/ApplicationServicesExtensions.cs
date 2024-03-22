6285cfed948ca05223be7cb6847cfeb52bbe30e9
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 09:40:14 +0500

    code: Application extensions methods

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Interfaces;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Errors;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Infa.Data;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Infa.Repositories;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage)
+                        .ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .WithOrigins("https://localhost:4200");
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ UnitOfWorkPrincipleInNetCoreWebAPI/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Extensions
+{
+    
+        public static class SwaggerServiceExtensions
+        {
+            public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+            {
+                services.AddEndpointsApiExplorer();
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EcommerceSportsShopNetAndAngular", Version = "v1" });
+                    var securitySchema = new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Auth Bearer Scheme",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    };
+                    c.AddSecurityDefinition("Bearer", securitySchema);
+                    var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {
+                        securitySchema, new[] { "Bearer" }
+                    }
+                };
+                    c.AddSecurityRequirement(securityRequirement);
+                });
+
+                return services;
+            }
+
+            public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceSportsShopNetAndAngular v1"));
+
+                return app;
+            }
+        }
+    
+}
