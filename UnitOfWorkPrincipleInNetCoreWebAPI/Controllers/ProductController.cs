03f598e1663ad0049cad5a49405b32ba3386bf2e
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 09:37:58 +0500

    code: product api controller with product spec.

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Controllers;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Interfaces;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Specifications;
+using UnitOfWorkPrincipleInNetCoreWebAPI.DTOS;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Errors;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Helpers;
+
+namespace EcommerceSportsShopNetAndAngular.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductsController(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+            [FromQuery] ProductSpecParams productParams)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductsWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _unitOfWork.Repository<Product>().CountAsync(countSpec);
+            var products = await _unitOfWork.Repository<Product>().ListAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,
+                productParams.PageSize, totalItems, data));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+            var product = await _unitOfWork.Repository<Product>().GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
+
+            return _mapper.Map<Product, ProductToReturnDto>(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _unitOfWork.Repository<ProductBrand>().ListAllAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            return Ok(await _unitOfWork.Repository<ProductType>().ListAllAsync());
+        }
+    }
+}
