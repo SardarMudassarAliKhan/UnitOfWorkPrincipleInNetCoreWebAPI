9c6d9b6abb878e8e169748ecfc43cbc6fd36d5ca
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 09:41:30 +0500

    code: application db context

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI.Infa/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Infa.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
