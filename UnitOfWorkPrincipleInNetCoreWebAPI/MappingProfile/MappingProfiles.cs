73e616ae5a3cf092ce376e17728c7411d434dae7
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 09:41:07 +0500

    code: Automapper mapping profiles

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI/MappingProfile/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+using UnitOfWorkPrincipleInNetCoreWebAPI.DTOS;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Helpers;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.MappingProfile
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>()).ReverseMap();
+        }
+    }
+}
