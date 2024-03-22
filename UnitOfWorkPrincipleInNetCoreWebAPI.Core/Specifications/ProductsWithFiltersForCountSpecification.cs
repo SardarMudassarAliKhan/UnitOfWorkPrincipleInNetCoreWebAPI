abb14ede6cd674caebc6c77969cb7c9ccbbecc63
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 07:33:43 +0500

    Code: Requred Specification Classes has been added in the code
    Ticket: #1881

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI.Core/Specifications/ProductsWithFiltersForCountSpecification.cs
@@ -0,0 +1,17 @@
+﻿using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Core.Specifications
+{
+    public class ProductsWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithFiltersForCountSpecification(ProductSpecParams productParams)
+            : base(x =>
+                           (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                           (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                           (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+                       )
+        {
+        }
+
+    }
+}
