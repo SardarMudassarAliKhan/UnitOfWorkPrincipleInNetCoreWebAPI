ff5b9ce8030fed4ea708234d18f16c2c733e67f5
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 07:32:32 +0500

    Code: Required Interfaces has been added  in the application
    Ticket:1881

---
+++ UnitOfWorkPrincipleInNetCoreWebAPI.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<Product> GetProductByNameAsync(string name);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
---
+++ UnitOfWorkPrincipleInNetCoreWebAPI.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using UnitOfWorkPrincipleInNetCoreWebAPI.Core.Model;
+
+namespace UnitOfWorkPrincipleInNetCoreWebAPI.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> Complete();
+    }
+}
