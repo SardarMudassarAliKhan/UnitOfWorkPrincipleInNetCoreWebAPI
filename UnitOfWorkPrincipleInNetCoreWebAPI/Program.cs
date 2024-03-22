3c0ff3230e406b4f1ad6a68e06e9a50a09808170
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-03-22 09:43:53 +0500

    config: Api layer configurations updations.

--- UnitOfWorkPrincipleInNetCoreWebAPI/Program.cs
+++ UnitOfWorkPrincipleInNetCoreWebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Extensions;
+using UnitOfWorkPrincipleInNetCoreWebAPI.Infa.Data; // Make sure to import the namespace for Entity Framework Core
 
 namespace UnitOfWorkPrincipleInNetCoreWebAPI
 {
@@ -6,29 +10,48 @@ namespace UnitOfWorkPrincipleInNetCoreWebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;           
 
-            // Add services to the container.
-
+            // Adding Services to the IOC container
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddSwaggerDocumentation();
 
             var app = builder.Build();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
 
-            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+            app.UseStaticFiles();
 
+            app.UseHttpsRedirection();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<AppDbContext>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occured during migration");
+            }
+
 
             app.Run();
         }
